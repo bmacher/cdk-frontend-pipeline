06331b494e9e7dd501fb9b98bf35e3c76ca74357
Benjamin Macher <benjamin.dev@icloud.com>
2020-11-13 16:27:27 +0100

    refactor

--- lib/repo-stack.ts
+++ lib/repo-stack.ts
@@ -2,18 +2,10 @@ import * as cdk from '@aws-cdk/core';
 import * as codecommit from '@aws-cdk/aws-codecommit';
 
 export class RepoStack extends cdk.Stack {
-  public readonly pipelineRepoArn: string;
-
   public readonly frontendRepoArn: string;
 
   constructor(scope: cdk.App, id: string) {
     super(scope, id);
-
-    const pipelineRepo = new codecommit.Repository(this, 'PipelineRepository', {
-      repositoryName: 'sfubt-pipeline',
-    });
-
-    this.pipelineRepoArn = pipelineRepo.repositoryArn;
 
     const frontendRepo = new codecommit.Repository(this, 'FrontendRepository', {
       repositoryName: 'sfubt-frontend',
